669176e28d76b24e6883ec871bd30e4032f644b1
wuzhi09 <34471145+cryanskl@users.noreply.github.com>
2022-09-15 11:05:04 +0800

    -m

---
+++ 0_Test/test.cs
@@ -0,0 +1,16 @@
+namespace QuizQuestions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+
+            for (i = 0; ;)
+            {
+                Console.WriteLine("hello");
+            }
+            Console.ReadLine();
+        }
+    }
+}
